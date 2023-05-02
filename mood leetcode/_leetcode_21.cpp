0d6aa09658b7cf5ae88620dcd4b95634c10a181a
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-05-02 11:31:58 +0530

    corrected some syntax errors

--- mood leetcode/_leetcode_21.cpp
+++ mood leetcode/_leetcode_21.cpp
@@ -8,6 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include <bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+int main()
+{
+    
+    
+    
+    return 0;
+}
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
--- mood leetcode/_leetcode_66_plusOne.cpp
+++ mood leetcode/_leetcode_66_plusOne.cpp
@@ -1,3 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+    
+    
+    
+    return 0;
+}
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
