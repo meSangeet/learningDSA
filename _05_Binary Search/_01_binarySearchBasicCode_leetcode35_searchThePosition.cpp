32a2d93251809145e1ef7b13aa82e0c5afc2f6f5
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-06-12 16:47:51 +0530

    added the code for binary search

--- _05_Binary Search/_01_binarySearchBasicCode_leetcode35_searchThePosition.cpp
+++ _05_Binary Search/_01_binarySearchBasicCode_leetcode35_searchThePosition.cpp
@@ -1,32 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
+
     int searchInsert(vector<int>& nums, int target) {
         int s = 0, l = nums.size() - 1;
         int mid = (s+l)>>1;
-        if(target > nums[l]) return l+1;
-        while(s<l)
+        while(s<=l)
         {
-            mid = (s+l)>>1;
+            
             if(nums[mid] > target)
             {
-                l = mid;
+                l = mid - 1;
             }else if(nums[mid] < target){
                 s = mid+1;
             }else{
                 return mid;
             }
+            mid = (s+l)>>1;
         }
-            return s;
+            return -1;
     }
-};
+
 
 int main()
 {
     
-    
+    int n; cout<<"enter nummber of elements in your array - "; cin>>n;
+    vector<int> arr;
+    for(int i = 0; i<n; i++)
+    {
+        int p; cin>>p;
+        arr.push_back(p);
+    }
+    int target; cout<<endl<<"enter the target - "; cin>>target;
+    if(searchInsert(arr, target) != -1)
+    {
+        cout<<endl<<"your target is present at the index "<<searchInsert(arr, target)<<endl;
+    }else{
+        cout<<endl<<"your array doesnot contain the target"<<endl;
+    }
     
     return 0;
 }
