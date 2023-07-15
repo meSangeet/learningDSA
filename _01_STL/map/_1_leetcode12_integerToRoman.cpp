5b6297a40e14548793a6b7b2ad687f1b79835984
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-15 22:09:55 +0530

    integer to roman

--- _01_STL/map/_1_leetcode12_integerToRoman.cpp
+++ _01_STL/map/_1_leetcode12_integerToRoman.cpp
@@ -19,50 +19,56 @@ string toRoman(int n)
     CM            900
     */
 
-   unordered_map<int, string> m;
-   m[0] = "";
-   m[1] = "I";
-   m[5] = "V";
-   m[10] = "X";
-   m[50] = "L";
-   m[100] = "C";
-   m[500] = "D";
-   m[1000] = "M";
-   m[4] = "IV";
-   m[9] = "IX";
-   m[40] = "XL";
-   m[90] = "XC";
-   m[400] = "CD";
-   m[900] = "CM";
-   int i = 1;
-   deque<string> str;
-   while(n != 0)
-   {
-    string curr = "";
-    int temp = n%10;
-    int mul = temp*i;
-    if(temp >= 5 && temp<9){
-    curr += m[5*i];
-    int fac = temp - 5;
-    while(fac--) curr += m[i];
-    }else if(temp >= 1 && temp<4)
+    unordered_map<int, string> m;
+    m[0] = "";
+    m[1] = "I";
+    m[5] = "V";
+    m[10] = "X";
+    m[50] = "L";
+    m[100] = "C";
+    m[500] = "D";
+    m[1000] = "M";
+    m[4] = "IV";
+    m[9] = "IX";
+    m[40] = "XL";
+    m[90] = "XC";
+    m[400] = "CD";
+    m[900] = "CM";
+    int i = 1;
+    deque<string> str;
+    while (n != 0)
     {
-    curr += m[i];
-    int fac = temp - 1;
-    while(fac--) curr += m[i]; 
-    }else{
-        curr += m[mul];
+        string curr = "";
+        int temp = n % 10;
+        int mul = temp * i;
+        if (temp >= 5 && temp < 9)
+        {
+            curr += m[5 * i];
+            int fac = temp - 5;
+            while (fac--)
+                curr += m[i];
+        }
+        else if (temp >= 1 && temp < 4)
+        {
+            curr += m[i];
+            int fac = temp - 1;
+            while (fac--)
+                curr += m[i];
+        }
+        else
+        {
+            curr += m[mul];
+        }
+        str.push_front(curr);
+        n /= 10;
+        i *= 10;
     }
-    str.push_front(curr);
-    n /= 10;
-    i *= 10;
-   } 
-   string ans;
-   for(int i =0; i<str.size(); i++)
-   {
-    ans += str[i];
-   }
-   return ans;
+    string ans;
+    for (int i = 0; i < str.size(); i++)
+    {
+        ans += str[i];
+    }
+    return ans;
 }
 
 int main()
@@ -70,6 +76,6 @@ int main()
 
     int n;
     cin >> n;
-    cout<<toRoman(n);
+    cout << toRoman(n);
     return 0;
-}S
+}
