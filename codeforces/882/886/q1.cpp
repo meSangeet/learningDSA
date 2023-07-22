195f1c2b60dc63df19f4d089333af1da1c9775c6
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-22 05:31:50 +0530

    added codeforces solutions round 886

---
+++ codeforces/882/886/q1.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+//macros
+#define mod 1000000007
+#define ll long long
+#define ld long double
+#define pub push_back
+#define pob pop_back
+#define pof pop_front
+#define puf push_front
+#define mp make_pair
+#define vi vector<ll>
+#define vp vector<pair<ll,ll>>
+#define vii vector<vi>
+#define pie 3.1415926535898
+#define pb push_back
+#define fr(n) for(ll iii = 0; iii<n; iii++)
+#define rv(n,v) fr(n){ll pppp; cin>>pppp; v.pub(pppp);}
+#define take(a) ll a; cin>>a;
+
+//methods
+ll mult(ll a, ll b, ll p = mod)
+{
+    return ((a % p) * (b % p)) % p;
+}
+ll add(ll a, ll b, ll p = mod)
+{
+    return ((a % p) + (b % p)) % p;
+}
+ll neg(ll a, ll p = mod)
+{
+    return (p - (a % p)) % p;
+}
+ll sub(ll a, ll b, ll p = mod)
+{
+    return add(a, neg(b));
+}
+ll fpow(ll x, ll y)
+{
+    ll res = 1;
+    x = x % mod;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1ll)
+            res = (res * x) % mod;
+        y = y >> 1ll;
+        x = (x * x) % mod;
+    }
+    return res;
+}
+ll inv(ll a, ll p = mod) { return fpow(a, p - 2); }
+
+//debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" -> "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template<class T> void _print(vector<T> v1)
+{
+    cerr << "[ ";
+    for(T i:v1){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<" ]";
+}
+
+template<class T> void _print(set<T> v1)
+{
+    cerr << "[ ";
+    for(T i:v1){
+        _print(i);
+        cerr<<" ";
+    }
+    cerr<<" ]";
+}
+
+
+template<class T, class M> void _print(pair<T,M> p)
+{
+    cerr << "{ " << p.first <<" "<<p.second<<" }";
+}
+
+template<class T, class M> void _print(map<T,M> p)
+{
+    for(auto x : p)
+    cerr << "{ " << x.first <<" "<<x.second<<" }"<<endl;
+}
+
+template<class T, class M> void _print(unordered_map<T,M> p)
+{
+    for(auto x : p)
+    cerr << "{ " << x.first <<" "<<x.second<<" }"<<endl;
+}
+
+
+//solve for multiple test cases
+void solve()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        ll a,b,c; cin>>a>>b>>c;
+        ll sum = a+b+c;
+        ll A = sum-a;
+        ll B = sum-b;
+        ll C = sum-c;
+        if(A >= 10 || B >= 10 || C >= 10)
+        {
+            cout<<"YES"<<"\n";
+        }else{
+            cout<<"NO"<<"\n";
+        }
+    }
+}
+
+//main function
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr); // to print debug into error.txt file
+    #endif
+    solve();
+    return 0;
+}
--- demo.cpp
+++ demo.cpp
@@ -1,193 +1,64 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-typedef unsigned long long ull;
-typedef long double lld;
+bool isBalanced(const std::vector<int>& difficulties, int d, int k) {
+    int n = difficulties.size();
+    int prev_difficulty = difficulties[0];
 
-// macros
-#define mod 1000000007
-#define ll long long
-#define ld long double
-#define pub push_back
-#define pob pop_back
-#define pof pop_front
-#define puf push_front
-#define mp make_pair
-#define vi vector<ll>
-#define vp vector<pair<ll, ll>>
-#define vii vector<vi>
-#define pie 3.1415926535898
-#define pb push_back
-#define fr(n) for (ll iii = 0; iii < n; iii++)
-#define rv(n, v)     \
-    fr(n)            \
-    {                \
-        ll pppp;     \
-        cin >> pppp; \
-        v.pub(pppp); \
-    }
-#define take(a) \
-    ll a;       \
-    cin >> a;
+    for (int i = 1; i < n; ++i) {
+        int current_difficulty = difficulties[i];
+        int diff = current_difficulty - prev_difficulty;
 
-// methods
-ll mult(ll a, ll b, ll p = mod)
-{
-    return ((a % p) * (b % p)) % p;
-}
-ll add(ll a, ll b, ll p = mod)
-{
-    return ((a % p) + (b % p)) % p;
-}
-ll neg(ll a, ll p = mod)
-{
-    return (p - (a % p)) % p;
-}
-ll sub(ll a, ll b, ll p = mod)
-{
-    return add(a, neg(b));
-}
-ll fpow(ll x, ll y)
-{
-    ll res = 1;
-    x = x % mod;
-    if (x == 0)
-        return 0;
-    while (y > 0)
-    {
-        if (y & 1ll)
-            res = (res * x) % mod;
-        y = y >> 1ll;
-        x = (x * x) % mod;
-    }
-    return res;
-}
-ll inv(ll a, ll p = mod) { return fpow(a, p - 2); }
-
-// debug
-#ifndef ONLINE_JUDGE
-#define debug(x)          \
-    cerr << #x << " -> "; \
-    _print(x);            \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t)
-{
-    cerr << t;
-}
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T>
-void _print(vector<T> v1)
-{
-    cerr << "[ ";
-    for (T i : v1)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << " ]";
-}
-
-template <class T>
-void _print(set<T> v1)
-{
-    cerr << "[ ";
-    for (T i : v1)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << " ]";
-}
-
-template <class T, class M>
-void _print(pair<T, M> p)
-{
-    cerr << "{ " << p.first << " " << p.second << " }";
-}
-
-template <class T, class M>
-void _print(map<T, M> p)
-{
-    for (auto x : p)
-        cerr << "{ " << x.first << " " << x.second << " }" << endl;
-}
-
-template <class T, class M>
-void _print(unordered_map<T, M> p)
-{
-    for (auto x : p)
-        cerr << "{ " << x.first << " " << x.second << " }" << endl;
-}
-
-// solve for multiple test cases
-void solve()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        take(n);
-        vi A;
-        vi B;
-        rv(n, A);
-        B = A;
-        ll count = 0, cnt = LONG_LONG_MAX;
-        for (int i = 0; i <= 2; i++)
-        {
-            for (int j = 0; j <= 2; j++)
-            {
-                ll increment = 0;
-
-                ll x = A[0], y = A[1];
-
-                while(x%3 != i)
-                {
-                    x++;
-                    increment++;
-                }
-                while(y%3 != j)
-                {
-                    y++;
-                    increment++;
-                }
-
-                for(int k = 2; k<n; k++)
-                {
-                    ll z = A[k];
-                    ll total = x+y+z;
-                    while(total%3 != 0)
-                    {
-                        z++;
-                        total++;
-                        increment++;
-                    }
-                    x = y;
-                    y = z;
-                }
-                cnt = min(increment, cnt);
-            }
+        if (diff > d) {
+            int num_removed = (diff - 1) / d;
+            k -= num_removed;
+            if (k < 0)
+                return false;
         }
 
-        cout << cnt << endl;
+        prev_difficulty = current_difficulty;
     }
+
+    return true;
 }
 
-// main function
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr); // to print debug into error.txt file
-#endif
-    solve();
+int minProblemsToRemove(std::vector<int>& difficulties, int d) {
+    int n = difficulties.size();
+    std::sort(difficulties.begin(), difficulties.end());
+
+    int left = 0, right = n - 1, result = n;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (isBalanced(difficulties, d, mid)) {
+            result = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        int n, d;
+        std::cin >> n >> d;
+
+        std::vector<int> difficulties(n);
+        for (int i = 0; i < n; ++i) {
+            std::cin >> difficulties[i];
+        }
+
+        int result = minProblemsToRemove(difficulties, d);
+        std::cout << result << std::endl;
+    }
+
     return 0;
 }
