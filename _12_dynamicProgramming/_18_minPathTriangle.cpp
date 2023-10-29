6a7a4b284a1226322f08ec29298d9ccf5e68e08d
Sangeet Sharma <sangeetworkspace@gmail.com>
2023-10-29 15:25:35 +0530

    triangular grid

---
+++ _12_dynamicProgramming/_18_minPathTriangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        for(int i = triangle.size()-2; i>=0; i--)
+        {
+            for(int j = 0; j<triangle[i].size(); j++)
+            {
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+            }
+        }
+
+        return triangle[0][0];
+    }
+};
+
+//main function
+int main()
+{
+
+    return 0;
+}
