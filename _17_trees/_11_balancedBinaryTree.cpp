5c310f8be04959df0947d2265d94a3c3ba2fdbff
Sangeet Sharma <sangeetworkspace@gmail.com>
2023-11-04 17:40:23 +0530

    balanced binary tree

---
+++ _17_trees/_11_balancedBinaryTree.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* temp)
+    {
+        if(!temp) return 0;
+
+        int left = height(temp->left);
+        int right = height(temp->right);
+
+        if(left == -1 || right == -1)
+        {
+            return -1;
+        }
+
+        int ans = abs(left-right);
+        if(ans > 1)
+        {
+            return -1;
+        }
+
+        return max(left, right) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(height(root) == -1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+};
+
+//main function
+int main()
+{
+
+    return 0;
+}
