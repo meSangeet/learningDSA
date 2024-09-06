e6cba613d47b723dbd094eeddb02b7bdb4e6ecf7
obtused <sangeetworkspace@gmail.com>
2024-09-06 22:42:56 +0530

    added two questions one from linked list and other from graphs

---
+++ _16_linkedlist/_03_leetcode_3417.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> toRemove(nums.begin(), nums.end());
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* current = &dummy;
+        while (current->next != nullptr) {
+            if (toRemove.find(current->next->val) != toRemove.end()) {
+                ListNode* nodeToRemove = current->next;
+                current->next = current->next->next;
+                delete nodeToRemove; 
+            } else {
+                current = current->next;
+            }
+        }
+        return dummy.next;
+    }
+};
