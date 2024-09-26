deabb57374542a607a882787e42e34417c504e69
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-09-26 23:28:55 +0530

    Time: 83 ms (51.25%) | Memory: 41.5 MB (97.04%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -6,21 +6,17 @@ public:
     }
     
     bool book(int start, int end) {
-        // Check each previously booked event for overlap
         for (int i = 0; i < bookings.size(); i++) {
             int booked_start = bookings[i].first;
             int booked_end = bookings[i].second;
             
-            // Check if the current event overlaps with any previously booked event
             if (start < booked_end && booked_start < end) {
-                // Overlap detected, return false (cannot book this event)
                 return false;
             }
         }
         
-        // If no overlap found, add the new event to the list of bookings
         bookings.push_back({start, end});
-        return true;  // Successfully booked the event
+        return true; 
     }
 };
 
