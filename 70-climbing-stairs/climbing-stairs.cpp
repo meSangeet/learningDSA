293ba9015139a611d6d458c68ce6548efa0925b4
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-01 20:40:20 +0530

    Time: 3 ms (29.68%) | Memory: 7.5 MB (69.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -6,20 +6,17 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> memo(n+1, -1);  // Memoization array initialized with -1
-        return dp(n, memo);
-    }
-    
-private:
-    int dp(int n, vector<int>& memo) {
-        // Base cases
         if (n <= 2) return n;
         
-        // If already computed, return memoized result
-        if (memo[n] != -1) return memo[n];
+        vector<int> dp(n+1);
+        dp[1] = 1;  // Base case: 1 way to climb 1 step
+        dp[2] = 2;  // Base case: 2 ways to climb 2 steps
         
-        // Compute and memoize the result
-        memo[n] = dp(n-1, memo) + dp(n-2, memo);
-        return memo[n];
+        // Build the solution bottom-up
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
