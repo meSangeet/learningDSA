ecb8362cc37ecd1cb6b7f623642104a36ebcd97a
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-01 21:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (73.94%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int robRange(vector<int>& nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int current = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        return max(robRange(nums, 0, n-2), robRange(nums, 1, n-1));
+    }
+};
