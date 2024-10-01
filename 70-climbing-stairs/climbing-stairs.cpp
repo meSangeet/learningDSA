64b2a67dac73607cb583b36246dacbf3adc97fd0
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-01 20:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (37.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,22 +1,7 @@
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
-        
-        vector<int> dp(n+1);
-        dp[1] = 1;  // Base case: 1 way to climb 1 step
-        dp[2] = 2;  // Base case: 2 ways to climb 2 steps
-        
-        // Build the solution bottom-up
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        
-        return dp[n];
+        const double phi = (1 + sqrt(5)) / 2;
+        return round(pow(phi, n+1) / sqrt(5));
     }
 };
