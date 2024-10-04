89b235e284270e064685b31c4144e05918183b2a
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-05 02:27:42 +0530

    Time: 54 ms (40.61%) | Memory: 40.2 MB (34.99%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -2,36 +2,26 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(n, INT_MIN)));
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        dp[0][0] = (grid[0][0] == 1) ? 1 : 0;
         
-        dp[n-1][n-1][n-1] = grid[n-1][n-1];
-        
-        for (int r1 = n-1; r1 >= 0; r1--) {
-            for (int c1 = n-1; c1 >= 0; c1--) {
-                for (int c2 = n-1; c2 >= 0; c2--) {
-                    int r2 = r1 + c1 - c2;
-                    if (r2 < 0 || r2 >= n) continue;
-                    if (grid[r1][c1] == -1 || grid[r2][c2] == -1) continue;
-                    
-                    if (r1 == n-1 && c1 == n-1) continue;
-                    
-                    int result = grid[r1][c1];
-                    if (r1 != r2) result += grid[r2][c2];
-                    
-                    int maxFuture = INT_MIN;
-                    if (r1 < n-1) maxFuture = max(maxFuture, dp[r1+1][c1][c2]);
-                    if (c1 < n-1) maxFuture = max(maxFuture, dp[r1][c1+1][c2]);
-                    if (r1 < n-1 && c2 < n-1) maxFuture = max(maxFuture, dp[r1+1][c1][c2+1]);
-                    if (c1 < n-1 && c2 < n-1) maxFuture = max(maxFuture, dp[r1][c1+1][c2+1]);
-                    
-                    if (maxFuture != INT_MIN) {
-                        result += maxFuture;
-                        dp[r1][c1][c2] = result;
+        for (int t = 1; t < 2*n - 1; ++t) {
+            vector<vector<int>> dp2(n, vector<int>(n, INT_MIN));
+            for (int i = max(0, t-(n-1)); i <= min(n-1, t); ++i) {
+                for (int j = max(0, t-(n-1)); j <= min(n-1, t); ++j) {
+                    if (grid[i][t-i] == -1 || grid[j][t-j] == -1) continue;
+                    int cherries = grid[i][t-i];
+                    if (i != j) cherries += grid[j][t-j];
+                    for (int pi = i-1; pi <= i; ++pi) {
+                        for (int pj = j-1; pj <= j; ++pj) {
+                            if (pi >= 0 && pj >= 0)
+                                dp2[i][j] = max(dp2[i][j], dp[pi][pj] + cherries);
+                        }
                     }
                 }
             }
+            dp = dp2;
         }
-        
-        return max(0, dp[0][0][0]);
+        return max(0, dp[n-1][n-1]);
     }
 };
