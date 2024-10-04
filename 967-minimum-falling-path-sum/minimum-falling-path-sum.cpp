3afcdd9f8ef6ce35bebef5f07609bf1715b6b3c5
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-05 02:22:46 +0530

    Time: 8 ms (88.79%) | Memory: 13.9 MB (59.02%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n));
         
+        // Initialize the bottom row
+        for (int j = 0; j < n; j++) {
+            dp[n-1][j] = matrix[n-1][j];
+        }
+        
+        // Build the dp table from bottom to top
         for (int i = n-2; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] += min({j > 0 ? matrix[i+1][j-1] : INT_MAX,
-                                     matrix[i+1][j],
-                                     j < n-1 ? matrix[i+1][j+1] : INT_MAX});
+                dp[i][j] = matrix[i][j] + min({j > 0 ? dp[i+1][j-1] : INT_MAX,
+                                               dp[i+1][j],
+                                               j < n-1 ? dp[i+1][j+1] : INT_MAX});
             }
         }
         
-        return *min_element(matrix[0].begin(), matrix[0].end());
+        // Return the minimum value in the top row
+        return *min_element(dp[0].begin(), dp[0].end());
     }
 };
