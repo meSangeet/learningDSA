b43eb9ff827c0fb73ac78153157c3e8bfd430b29
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-05 02:14:14 +0530

    Time: 11 ms (50.24%) | Memory: 14.2 MB (60.94%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
+        vector<int> dp(n, INT_MAX);
         
-        for (int i = 1; i < m; i++)
-            grid[i][0] += grid[i-1][0];
+        dp[0] = 0;
         
-        for (int j = 1; j < n; j++)
-            grid[0][j] += grid[0][j-1];
-        
-        for (int i = 1; i < m; i++) {
+        for (int i = 0; i < m; i++) {
+            dp[0] += grid[i][0];
             for (int j = 1; j < n; j++) {
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+                dp[j] = grid[i][j] + min(dp[j], dp[j-1]);
             }
         }
         
-        return grid[m-1][n-1];
+        return dp[n-1];
     }
 };
