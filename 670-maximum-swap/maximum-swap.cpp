57d07c1561e4c0fc31c69ef03afb89b7c7e01a25
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-17 18:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (89.76%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-            // Convert the number into a string of digits for easy manipulation
-    string digits = to_string(num);
-    int n = digits.size();
-    
-    // Create an array to store the index of the maximum digit to the right of each digit
-    vector<int> maxIdx(n);
-    
-    // Fill the maxIdx array, where maxIdx[i] will be the index of the largest digit
-    // starting from i to the end
-    int maxIndex = n - 1;
-    maxIdx[maxIndex] = maxIndex;
-    
-    for (int i = n - 2; i >= 0; --i) {
-        if (digits[i] > digits[maxIndex]) {
-            maxIndex = i;
+        string strnum = to_string(num);
+        int n = strnum.length();
+        vector<int> pref(n);
+        int maax = n-1;
+        pref[maax] = n-1;
+        for(int i = n-2; i>=0; i--){
+            if(strnum[i] > strnum[maax]){
+                pref[i] = i;
+                maax = i;
+            }else{
+                pref[i] = maax;
+            }
         }
-        maxIdx[i] = maxIndex;
-    }
-    
-    // Now find the first position where a swap will give a larger number
-    for (int i = 0; i < n; ++i) {
-        // If the current digit is smaller than the maximum digit to its right, swap
-        if (digits[i] < digits[maxIdx[i]]) {
-            // Swap digits[i] with digits[maxIdx[i]]
-            swap(digits[i], digits[maxIdx[i]]);
-            break;
+        unordered_map<int,int> m;
+        for(int i = 0; i<n; i++){
+            if(strnum[i] < strnum[pref[i]]){
+                swap(strnum[i], strnum[pref[i]]);
+                break;
+            }
         }
-    }
-    
-    // Convert the modified digits string back to an integer and return it
-    return stoi(digits);
+        return stoi(strnum);
     }
 };
