01e75d39ed365868cd3143e6d55d2005d9858116
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-18 00:26:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (81.67%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,62 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        
-        if (obstacleGrid[0][0] == 1) return 0;
-        obstacleGrid[0][0] = 1;
-        
-        for (int i = 1; i < m; i++)
-            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1) ? 1 : 0;
-        
-        for (int j = 1; j < n; j++)
-            obstacleGrid[0][j] = (obstacleGrid[0][j] == 0 && obstacleGrid[0][j-1] == 1) ? 1 : 0;
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0)
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                else
-                    obstacleGrid[i][j] = 0;
-            }
-        }
-        
-        return obstacleGrid[m-1][n-1];
+        // int n = obstacleGrid.size();
+        // int m = obstacleGrid[0].size();
+        // if(obstacleGrid[0][0] == 1) return 0;
+        // bool flag = false;
+        // for(int i = 0; i<m; i++){
+        //     if(obstacleGrid[0][i] == 0 && !flag){
+        //         obstacleGrid[0][i] = 1;
+        //     }else{
+        //         obstacleGrid[0][i] = 0;
+        //         flag = true;
+        //     }
+        // }
+        // flag = false;
+        // for(int i = 1; i<n; i++){
+        //     if(obstacleGrid[i][0] == 0 && !flag){
+        //         obstacleGrid[i][0] = 1;
+        //     }else{
+        //         obstacleGrid[i][0] = 0;
+        //         flag = true;
+        //     }
+        // }
+        // for(int i = 1; i<n; i++){
+        //     for(int j = 1; j<m; j++){
+        //         if(obstacleGrid[i][j] == 1){
+        //             obstacleGrid[i][j] = 0;
+        //         }else{
+        //             obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+        //         }
+        //     }
+
+        // }
+        // return obstacleGrid[n-1][m-1];
+         int m = obstacleGrid.size();
+   int n = obstacleGrid[0].size();
+  
+   // Edge case: If the starting point or the ending point is an obstacle
+   if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+       return 0;
+   }
+  
+   // dp array to store the number of unique paths
+   vector<int> dp(n, 0);
+   dp[0] = 1; // Starting point
+  
+   // Traverse through the grid
+   for (int i = 0; i < m; i++) {
+       for (int j = 0; j < n; j++) {
+           if (obstacleGrid[i][j] == 1) {
+               dp[j] = 0; // No path through an obstacle
+           } else if (j > 0) {
+               dp[j] += dp[j - 1]; // Update dp[j] by adding the number of paths from the left
+           }
+       }
+   }
+  
+   return dp[n - 1]; // The bottom-right corner will have the answer
+
     }
 };
