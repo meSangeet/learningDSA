0777192b7d6ed5cb4d7b47c369840c7fb95c8484
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-17 23:32:40 +0530

    Time: 2 ms (39.90%) | Memory: 9.5 MB (45.62%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -22,7 +22,17 @@ int rob(vector<int>& nums) {
     //f(i) = answer till ith index so we need to find f(n-1)
     
     //base case
-    vector<int> dp(n,-1);
-    return fun(nums, n-1, dp);
+    vector<int> dp(n+2, 0);
+
+    for(int i = 2; i<=n+1; i++){
+        //case 1
+        int c1 = nums[i-2] + dp[i-2]; 
+        //case 2
+        int c2 = dp[i-1];
+
+        dp[i] = max(c1,c2);
+    }
+
+    return dp[n+1];
 }
 };
