174a86bca61b69d1d893311cb130f0787ba4f5b7
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-10-30 08:54:13 +0530

    Time: 93 ms (94.92%) | Memory: 12.3 MB (67.67%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int dp(int left, int right, vector<int>& nums, vector<vector<int>>& memo) {
-        if (left + 1 == right) return 0; // No balloon to burst
-        if (memo[left][right] != -1) return memo[left][right];
-        int maxCoins = 0;
-        for (int k = left + 1; k < right; ++k) {
-            int coins = nums[left] * nums[k] * nums[right] + dp(left, k, nums, memo) + dp(k, right, nums, memo);
-            maxCoins = max(maxCoins, coins);
-        }
-        memo[left][right] = maxCoins;
-        return maxCoins;
-    }
-
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        vector<vector<int>> memo(n + 2, vector<int>(n + 2, -1));
-        return dp(0, n + 1, nums, memo);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int length = 2; length < n + 2; ++length) { // length of subarray
+            for (int left = 0; left + length < n + 2; ++left) {
+                int right = left + length;
+                for (int k = left + 1; k < right; ++k) {
+                    dp[left][right] = max(dp[left][right], nums[left] * nums[k] * nums[right] + dp[left][k] + dp[k][right]);
+                }
+            }
+        }
+        return dp[0][n + 1];
     }
 };
