0a49b8d1c092a03ef99adbaaa6c1a28bee4eaa93
Sangeet Sharma <sangeetworkspace@gmail.com>
2024-12-30 02:26:46 +0530

    Time: 94 ms (31.87%) | Memory: 83.6 MB (20.68%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -4,25 +4,10 @@ public:
     const int MOD = 1e9 + 7;
     int n, m;
 
-    int countWays(int i, int j, const string& target, const vector<vector<int>>& count, vector<vector<ll>> &dp) {
-        if (i == target.size()) return 1; 
-        if (j == count[0].size()) return 0; 
-        if(dp[i][j] != -1) return dp[i][j];
-        //case 1
-        int skip = countWays(i, j + 1, target, count, dp);
-        //case2 - pick
-        int use = 0;
-        
-        if (count[target[i] - 'a'][j] > 0) {
-            use = (1LL * count[target[i] - 'a'][j] * countWays(i + 1, j + 1, target, count, dp)) % MOD;
-        }
-        return dp[i][j] = (skip + use) % MOD;
-    }
-
     int numWays(vector<string>& words, string target) {
         m = target.size();
         n = words[0].size();
-        vector<vector<ll>> dp(m+1, vector<ll> (n+1, -1));
+        vector<vector<ll>> dp(m+1, vector<ll> (n+1, 0));
         vector<vector<int>> count(26, vector<int>(n, 0));
         for (const string& word : words) {
             for (int j = 0; j < n; ++j) {
@@ -32,6 +17,24 @@ public:
             }
         }
 
-        return countWays(0, 0, target, count, dp);
+        //writing the base 
+        for(int i = 0; i<=n; i++) dp[0][i] = 1;
+
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                //not using the current column
+                dp[i][j] = dp[i][j-1];
+
+                //using the current column
+                if(count[target[i-1] - 'a'][j-1] > 0){
+                    dp[i][j] += dp[i-1][j-1]*(count[target[i-1] - 'a'][j-1]);
+                }
+
+                dp[i][j] %= MOD;
+            }
+        }
+
+
+        return dp[m][n];
     }
 };
