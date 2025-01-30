623ea61bf0cfa68945d392b478c3e9380518300c
Sangeet Sharma <sangeetworkspace@gmail.com>
2025-01-31 01:22:53 +0530

    Time: 65 ms (20.75%) | Memory: 44.2 MB (26.22%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,49 +1,41 @@
 class Solution {
-private:
-    bool isExit(int r, int c, int n, int m, int a, int b){
-        
-        if(r==0 || c==0 || r==n-1 || c==m-1){
-            
-            if(r!=a || c!=b){
-                return true;
-            } 
-        }
-        return false;
-    }
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int n = maze.size();
         int m = maze[0].size();
+        int ex = entrance[0];
+        int ey = entrance[1];
+        int lev = 0;
+        queue<pair<pair<int,int>,int>> q;
+        vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 
-        queue<pair<pair<int, int>, int>> q; // row, col
+        set<pair<int,int>> visited;
+        visited.insert({ex,ey});
 
-        q.push({{entrance[0], entrance[1]}, 0});
-
-        maze[entrance[0]][entrance[1]] = '*'; // vis
-
-        int delr[] = {0, 1, 0, -1};
-        int delc[] = {1, 0, -1, 0};
-
+        q.push({{ex,ey},0});
         while(!q.empty()){
-            auto cell = q.front();
-            q.pop();
-
-            int r = cell.first.first;
-            int c = cell.first.second;
-            int curr = cell.second;
-            
-
-            if(isExit(r, c, n, m, entrance[0], entrance[1])){
-                return curr;
+            auto temp = q.front();
+            int x = temp.first.first;
+            int y = temp.first.second;
+            int lev = temp.second;
+            if(x != ex || y != ey){
+                if(x == 0 || y == 0 || x == n-1 || y == m-1){
+                    return lev;
+                }
             }
-
-            for(int i=0; i<4; i++){
-                int nr = delr[i] + r;
-                int nc = delc[i] + c;
-
-                if(nr>=0 && nc>=0 && nr<n && nc<m && maze[nr][nc]=='.'){
-                    maze[nr][nc] = '*';
-                    q.push({{nr, nc}, curr+1});
+            q.pop();
+            for(int i = 0; i<4; i++){
+                int nx = x + dir[i].first;
+                int ny = y + dir[i].second;
+                if(visited.count({nx,ny})){
+                    //maa chudaye
+                }else{
+                    if(nx >= 0 and ny >= 0 and nx < n and ny < m){
+                        if(maze[nx][ny] == '.'){
+                            visited.insert({nx,ny});
+                            q.push({{nx,ny}, lev+1});
+                        }
+                    }
                 }
             }
         }
