a86aee33f0016769015d060ccad54ab583552b42
Sangeet Sharma <sangeetworkspace@gmail.com>
2025-02-04 14:58:21 +0530

    Time: 1965 ms (6.15%) | Memory: 640.4 MB (18.40%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,44 +1,41 @@
 class Solution {
-    bool check(string str, string tar) {
+
+    bool check(string str, string tar){
         int n = tar.length();
-        if (str.length() != n - 1) return false; // Ensure str is one character shorter than tar
+        if(str.length() != n-1) return false;
 
         bool flag = false;
         int ind = 0;
-        for (int i = 0; i < n; i++) {
-            if (ind < str.length() && str[ind] == tar[i]) {
-                ind++; // Match the character in both strings
-            } else {
-                if (flag) return false; // If we've already skipped a character in tar, return false
-                flag = true; // Mark that we skipped a character
+        for(int i = 0; i<n-1; i++){
+            if(str[i] != tar[ind]){
+                if(flag) return false;
+                flag = true;
+                ind++;
+                if(str[i] != tar[ind]) return false;
             }
+            ind++;
         }
+
         return true;
     }
-
 public:
-    static bool comp(string &a, string &b) {
+    static bool comp(string &a, string &b){
         return a.length() < b.length();
     }
-
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), comp); // Sort words by length
+        sort(words.begin(), words.end(), comp);
         int n = words.size();
         vector<int> dp(n, 1);
         int ans = 1;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (check(words[j], words[i])) { // Check if words[j] can form words[i]
+        for(int i = 1; i<n; i++){
+            for(int j = i-1; j>=0; j--){
+                if(check(words[j],words[i])){
                     dp[i] = max(dp[i], 1 + dp[j]);
                 }
             }
             ans = max(ans, dp[i]);
         }
-
-        // Optional: For debugging purposes
-        // for (auto x : dp) cout << x << " ";
-
+        for(auto x : dp) cout<<x<<" ";
         return ans;
     }
 };
