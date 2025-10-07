7cd9ac2a3c3849456e4d05cd3f7fdbac864f874a
Sangeet Sharma <sangeetworkspace@gmail.com>
2025-10-08 02:31:43 +0530

    Time: 213 ms (14.25%) | Memory: 104.8 MB (89.17%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -1,46 +1,40 @@
-// this variant version uses different UnionFind class
-class UnionFind{
-public:
-    vector<int> root;
-    UnionFind(vector<int>& rains, int n){
-        root.resize(n+1);
-        root[n]=n;
-        for(int i=n-1; i>=0; i--)
-            root[i]=(rains[i])?root[i+1]:i;
-    }
-    int Find(int x){
-        return (x==root[x])?x:root[x]=Find(root[x]);
-    }
-    void UnionNext(int x){
-        root[x]=Find(x+1);// union x & x+1
-    }
-};
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        const int n=rains.size();
-
-        // root is the 1st day dry after a succesive rainy days
-        UnionFind G(rains, n); 
-        unordered_map<int, int> rainDay;//lake->last rainy day
-        rainDay.reserve(n);
-        vector<int> ans(n, 1);// dry lake 1 by default
-        for(int i=0; i<n; i++){
-            const int lake=rains[i];
-            if (lake>0){
-                ans[i]=-1;
-                auto it=rainDay.find(lake);
-                if (it!=rainDay.end()){
-                    int prev=it->second;
-                    int dry=G.Find(prev+1); //find next dry day after prev
-                    if (dry>=i) return {};  //no dry day before i
-                    ans[dry]=lake;      // dry that lake
-                    G.UnionNext(dry);   // dry day as used
-                    it->second=i;       // update last rain
+        int n = rains.size();
+        vector<int> ans(n, -1);
+        // vector<int> dry;
+        unordered_map<int,int> mp;
+        vector<int> dry;    
+        for(int i = 0 ;i < n ; i++){
+            if(rains[i] == 0){
+                dry.push_back(i);
+            }
+            else{
+                if(mp.count(rains[i]) == 0){ //mtlb vo barish ka din pehli baar aaya
+                    mp[rains[i]] = i;
                 }
-                else rainDay[lake]=i;
-            } 
+                else{ // mtlb vo barish ka din dubara aaya
+                    if(dry.size() == 0) return {};
+                    // int dry_day = dry.front();
+                    int last_rain_day = mp[rains[i]];
+                    auto dry_day = upper_bound(dry.begin(), dry.end(), last_rain_day);
+                    if(dry_day == dry.end()){
+                        return  {};
+                    }
+                    else{
+                        int day = *dry_day;
+                        ans[day] = rains[i];
+                        mp[rains[i]] = i;
+                        dry.erase(dry_day);
+                    }
+                }
+            }
         }
-        return ans;
+        for(int i = 0 ; i < n ; i++){
+            if(rains[i] == 0 && ans[i] == -1)
+            ans[i] = 10001;
+        }
+        return ans;   
     }
 };
