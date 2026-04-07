8d4a7d6309c0516d8ac2aefb0e216d8b0dc84cd4
Sangeet Sharma <sangeetworkspace@gmail.com>
2026-04-08 00:27:58 +0530

    Time: 28 ms (67.24%) | Memory: 128.9 MB (8.62%) - LeetSync

---
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -0,0 +1,45 @@
+class Robot {
+public:
+    vector<vector<int>> pos;
+    int idx = 0;
+    bool move = false;
+    Robot(int width, int height) {
+        for(int x=0;x<width;x++){
+            pos.push_back({x,0,0});
+        }
+
+        for(int y=1;y<height;y++){
+            pos.push_back({width-1,y,1});
+        }
+
+        for(int x=width-2;x>=0;x--){
+            pos.push_back({x,height-1,2});
+        }
+
+        for(int y=height-2;y>0;y--){
+            pos.push_back({0,y,3});
+        }  
+
+        pos[0][2] = 3;      
+    }
+    
+    void step(int num) {
+        move = true;
+        idx = (idx+num)%pos.size();
+    }
+    
+    vector<int> getPos() {
+        return {pos[idx][0],pos[idx][1]};
+    }
+    
+    string getDir() {
+        if(!move){
+            return "East";
+        }
+        int d = pos[idx][2];
+        if(d == 0) return "East";
+        else if(d == 1) return "North";
+        else if(d == 2) return "West";
+        return "South";
+    }
+};
